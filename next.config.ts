1ad4196e54e066bd3e331b999a6bd31a4a4a7f1a
milad ramavandi <miladramavandi@milads-MacBook-Air.local>
2024-11-19 16:42:28 +0330

    next.config.ts and eslintrc.json new committed

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images:{
-    remotePatterns:[
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
       {
-        protocol:'https',
-        hostname:'links.papareact.com',
-        pathname:'/**'
+        protocol: "https",
+        hostname: "links.papareact.com",
+        pathname: "/**",
       },
       {
-        protocol:'https',
-        hostname:'logos-world.net',
-        pathname:'/**'
-      }
-    ]
-  }
+        protocol: "https",
+        hostname: "logos-world.net",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;

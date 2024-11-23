f016fff27db41a201667ae3d41d741382b1c1784
milad ramavandi <miladramavandi@milads-MacBook-Air.local>
2024-11-23 11:37:13 +0330

    page.tsx added some css properties

--- src/components/posts-feed/index.tsx
+++ src/components/posts-feed/index.tsx
@@ -8,7 +8,7 @@ import { Avatar, AvatarGroup } from "@nextui-org/react";
 const PostsFeed = () => {
   const { data } = useGetPosts();
   return (
-    <div className="space-y-4 pb-20 h-screen overflow-auto scrollbar-hide">
+    <div className="space-y-4 pb-20">
       {data
         ?.slice(0)
         .reverse()

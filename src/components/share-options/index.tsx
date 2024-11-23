27b97d325c05a03120f5b0c48780edad703e0a16
milad ramavandi <miladramavandi@milads-MacBook-Air.local>
2024-11-23 12:32:11 +0330

    share-options component updated

--- src/components/share-options/index.tsx
+++ src/components/share-options/index.tsx
@@ -17,7 +17,7 @@ const ShareOptions = () => {
     <div className="flex mt-2 space-x-1">
       <div title="Telegram">
         <TelegramShareButton
-          url={"https://github.com/milad-ramavandi/linkedin-clone"}
+          url={"https://linkedin-clone-three.vercel.app"}
           title={"Welcome to Likedin Clone"}
         >
           <TelegramIcon size={32} round xlinkTitle="Telegram" />
@@ -25,7 +25,7 @@ const ShareOptions = () => {
       </div>
       <div title="Whatsapp">
         <WhatsappShareButton
-          url={"https://github.com/milad-ramavandi/linkedin-clone"}
+          url={"https://linkedin-clone-three.vercel.app"}
           title={"Welcome to Likedin Clone"}
         >
           <WhatsappIcon size={32} round />
@@ -33,7 +33,7 @@ const ShareOptions = () => {
       </div>
       <div title="Linkedin">
         <LinkedinShareButton
-          url={"https://github.com/milad-ramavandi/linkedin-clone"}
+          url={"https://linkedin-clone-three.vercel.app"}
           title={"Welcome to Likedin Clone"}
         >
           <LinkedinIcon size={32} round />
@@ -41,7 +41,7 @@ const ShareOptions = () => {
       </div>
       <div title="Twitter">
         <TwitterShareButton
-          url={"https://github.com/milad-ramavandi/linkedin-clone"}
+          url={"https://linkedin-clone-three.vercel.app"}
           title={"Welcome to Likedin Clone"}
         >
           <TwitterIcon size={32} round />
